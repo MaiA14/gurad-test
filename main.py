bc53dd2ad8bb6c8a54e0460b0a01f036764f21cb
Mai <mai@Drors-MacBook-Pro-2.local>
2024-08-05 22:02:30 +0300

    probuf

--- main.py
+++ main.py
@@ -2,8 +2,31 @@ from typing import Dict, Union
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import requests  
+import json
+import pokedex_pb2 
 
 app = FastAPI()
+
+def decode_protobuf_bytes_to_json(protobuf_data: bytes) -> str:
+    pokemon = pokedex_pb2.Pokemon()
+    pokemon.ParseFromString(protobuf_data)
+    pokemon_dict = {
+        "number": pokemon.number,
+        "name": pokemon.name,
+        "type_one": pokemon.type_one,
+        "type_two": pokemon.type_two,
+        "total": pokemon.total,
+        "hit_points": pokemon.hit_points,
+        "attack": pokemon.attack,
+        "defense": pokemon.defense,
+        "special_attack": pokemon.special_attack,
+        "special_defense": pokemon.special_defense,
+        "speed": pokemon.speed,
+        "generation": pokemon.generation,
+        "legendary": pokemon.legendary
+    }
+    
+    return json.dumps(pokemon_dict, indent=2)
 
 @app.get("/")
 def read_root():
@@ -17,7 +40,11 @@ async def stream(request: Request):
         raise HTTPException(status_code=400, detail="Signature missing")
     body = await request.body()
 
+
     print('body ', body)
+
+    dump = decode_protobuf_bytes_to_json(body)
+    print('dump ', dump)
 
 
 @app.post("/stream_start")
@@ -34,3 +61,4 @@ async def stream_start():
         response = requests.post(url, json=payload)
     except Exception as e:
         return {"error": str(e)}
+
