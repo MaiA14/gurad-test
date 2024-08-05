0cbd41a1a2059209e51201ae58889358587dfe9b
Mai <mai@Drors-MacBook-Pro-2.local>
2024-08-05 22:40:43 +0300

    try probuf

--- main.py
+++ main.py
@@ -2,8 +2,30 @@ from typing import Dict, Union
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import requests  
+import pokedex_pb2
 
 app = FastAPI()
+
+def decode_protobuf_bytes_to_json(protobuf_data: bytes) -> str:
+    pokemon = pokedex_pb2.Pokemon()
+    pokemon.ParseFromString(protobuf_data)
+    pokemon_dict = {
+        "number": pokemon.number,
+        "name": pokemon.name,
+        "type_one": pokemon.type_one,
+        "type_two": pokemon.type_two,
+        "total": pokemon.total,
+        "hit_points": pokemon.hit_points,
+        "attack": pokemon.attack,
+        "defense": pokemon.defense,
+        "special_attack": pokemon.special_attack,
+        "special_defense": pokemon.special_defense,
+        "speed": pokemon.speed,
+        "generation": pokemon.generation,
+        "legendary": pokemon.legendary
+    }
+    
+    return json.dumps(pokemon_dict, indent=2)
 
 @app.get("/")
 def read_root():
@@ -16,7 +38,12 @@ async def stream(request: Request):
     if not signature:
         raise HTTPException(status_code=400, detail="Signature missing")
     body = await request.body()
+
     print('body ', body)
+
+    decoded_pokemon = decode_protobuf_bytes_to_json(body)
+
+    print('decoded_pokemon ', decoded_pokemon)
 
 
 @app.post("/stream_start")
