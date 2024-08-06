5593a4135c98d8068ec1f45e5034da24e4b6cde9
Mai <mai@Drors-MacBook-Pro-2.local>
2024-08-06 15:41:54 +0300

    validate header

--- main.py
+++ main.py
@@ -14,24 +14,33 @@ class StreamHandler:
     def read_root():
         return {"message": "Hi"}
 
-    @staticmethod
-    @app.post("/stream")
-    async def stream(request: Request):
-        headers = request.headers
-        print('headers ', headers)
-        body = await request.body()
-        print('body ', body)
-        
-        email = Config.get_stream_config_value("email")
-        key_base64 = Utils.get_secret(email)
-        key = base64.b64decode(key_base64)       
-        hmaci = HMAC.new(key, body, digestmod=SHA256).hexdigest()
 
-        print('hmaci ', hmaci)
+@app.post("/stream")
+async def stream(request: Request):
+    headers = request.headers
+    print('headers ', headers)
+    body = await request.body()
+    print('body ', body)
 
-        decoded_pokemon = Utils.decode_protobuf_bytes_to_json(body)
-        print('decoded ', decoded_pokemon)
-        return JSONResponse(content={"decoded_pokemon": decoded_pokemon})
+    signature = headers.get('x-grd-signature')
+
+    if signature is None:
+        raise HTTPException(status_code=400, detail='Missing x-grd-signature header')
+
+    email = Config.get_stream_config_value("email")
+    key_base64 = Utils.get_secret(email)
+    key = base64.b64decode(key_base64)
+    hmaci = HMAC.new(key, body, digestmod=SHA256).hexdigest()
+
+    if signature != hmaci:
+        raise HTTPException(status_code=403, detail='Invalid signature')
+
+    print('hmaci ', hmaci)
+
+    decoded_pokemon = Utils.decode_protobuf_bytes_to_json(body)
+    print('decoded ', decoded_pokemon)
+
+    return JSONResponse(content={"decoded_pokemon": decoded_pokemon})
 
     @staticmethod
     @app.post("/stream_start")
